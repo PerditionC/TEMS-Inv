c554aceb2eeb5e6dbcd8752c1d8103eef7e1597b
Kenneth Davis <PerditionC@gmail.com>
2019-04-21 16:30:22 -0400

    cleanup - remove unnecessary using statements

--- TEMS/views/asset management/GeneralInventoryManagementWindow.xaml.cs
+++ TEMS/views/asset management/GeneralInventoryManagementWindow.xaml.cs
@@ -2,18 +2,6 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using NLog;
 using TEMS_Inventory.views;
 
 namespace TEMS_Inventory
